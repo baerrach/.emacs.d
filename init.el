ad898c9873facabe8c23f981536ac0b88dc6b031
Barrie Treloar <baerrach@gmail.com>
2015-12-22 22:15:46 +1030

    Add backup and place saving

--- init.el
+++ init.el
@@ -27,3 +27,16 @@
 
 ;; Set up appearance early
 (require 'appearance)
+
+;; Write backup files to own directory
+(setq backup-directory-alist
+      `(("." . ,(expand-file-name
+                 (concat user-emacs-directory "backups")))))
+
+;; Make backups of files, even when they're in version control
+(setq vc-make-backup-files t)
+
+;; Save point position between sessions
+(require 'saveplace)
+(setq-default save-place t)
+(setq save-place-file (expand-file-name ".places" user-emacs-directory))
