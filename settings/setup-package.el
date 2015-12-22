5917c02833af39e42aece20c44b06fb03df03fc2
Barrie Treloar <baerrach@gmail.com>
2015-12-23 07:52:10 +1030

    Initialize package. Needed for access to variables

--- settings/setup-package.el
+++ settings/setup-package.el
@@ -6,8 +6,6 @@
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
 
-(package-initialize)
-
 (unless (file-exists-p "~/.emacs.d/elpa/archives/melpa")
   (package-refresh-contents))
 
