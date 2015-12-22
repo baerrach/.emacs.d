59cb40bfd729cdb3ae17e0590e0937d673f4134f
Barrie Treloar <baerrach@gmail.com>
2015-12-22 20:12:29 +1030

    Add site-lisp/ and settings/ to load path

--- init.el
+++ init.el
@@ -9,3 +9,14 @@
 
 ;; No splash screen please ... jeez
 (setq inhibit-startup-message t)
+
+;; Set path to dependencies
+(setq site-lisp-dir
+      (expand-file-name "site-lisp" user-emacs-directory))
+
+(setq settings-dir
+      (expand-file-name "settings" user-emacs-directory))
+
+;; Set up load path
+(add-to-list 'load-path settings-dir)
+(add-to-list 'load-path site-lisp-dir)
