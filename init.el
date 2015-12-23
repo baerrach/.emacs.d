b7183a3091191c06e3af4e4171f4bae407f6d144
Barrie Treloar <baerrach@gmail.com>
2015-12-24 00:03:56 +1030

    Add smartparens

--- init.el
+++ init.el
@@ -119,6 +119,19 @@
 (require 'setup-html-mode)
 (require 'setup-paredit)
 
+;; Default setup of smartparens
+(require 'smartparens-config)
+(setq sp-autoescape-string-quote nil)
+(--each '(css-mode-hook
+          restclient-mode-hook
+          js-mode-hook
+          java-mode
+          ruby-mode
+          markdown-mode
+          groovy-mode
+          scala-mode)
+  (add-hook it 'turn-on-smartparens-mode))
+
 (global-set-key (kbd "C-x g") 'magit-status)
 (global-set-key (kbd "C-x M-g") 'magit-dispatch-popup)
 
