248741d8505393a13f1e380e72c4aed516aca6e2
Barrie Treloar <baerrach@gmail.com>
2015-12-23 15:40:17 +1030

    Set theme to tango-dark

--- custom.el
+++ custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (tango-dark)))
  '(markdown-coding-system (quote utf-8))
  '(markdown-command "/usr/bin/redcarpet --parse-autolink")
  '(markdown-command-needs-filename t))
