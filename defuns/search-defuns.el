e599ad58eda298f691957245b8c53997df1aa9c2
Barrie Treloar <baerrach@gmail.com>
2016-01-07 10:07:58 +1030

    Delete unused isearch functions

--- defuns/search-defuns.el
+++
@@ -1,6 +0,0 @@
-(defun isearch-forward-use-region ()
-  (interactive)
-  (when (region-active-p)
-    (add-to-history 'search-ring (region-as-string))
-    (deactivate-mark))
-  (call-interactively 'isearch-forward))
