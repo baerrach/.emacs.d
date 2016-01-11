e54ef7ab2a4e16c47b82751744fb34982306758c
Barrie Treloar <baerrach@gmail.com>
2016-01-11 12:07:05 +1030

    Fix goto-line-with-feedback being undefined.

--- defuns/buffer-defuns.el
+++ defuns/buffer-defuns.el
@@ -15,3 +15,12 @@ Including indent-buffer, which should not be called automatically on save."
   (untabify-buffer)
   (delete-trailing-whitespace)
   (indent-buffer))
+
+(defun goto-line-with-feedback ()
+  "Show line numbers temporarily, while prompting for the line number input"
+  (interactive)
+  (unwind-protect
+      (progn
+        (linum-mode 1)
+        (call-interactively 'goto-line))
+    (linum-mode -1)))
