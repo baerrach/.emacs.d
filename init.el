bda49f0b03af811e2eb7fafdbfc1ca87ac9967dd
Barrie Treloar <baerrach@gmail.com>
2017-01-25 09:32:17 +1030

    Disable bell on exceeding scroll limit

--- init.el
+++ init.el
@@ -192,3 +192,12 @@
 
 ;; Setup key bindings
 (require 'key-bindings)
+
+;; Disable bell on scrolling past limit
+(defun my-bell-function ()
+  (unless (memq this-command
+        '(isearch-abort abort-recursive-edit exit-minibuffer
+              keyboard-quit mwheel-scroll down up next-line previous-line
+              backward-char forward-char))
+    (ding)))
+(setq ring-bell-function 'my-bell-function)
