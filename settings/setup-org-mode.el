37562ce953fea5d8f841f98ad22d854057355c8b
Barrie Treloar <baerrach@gmail.com>
2017-01-31 10:18:11 +1030

    Modify html export to subtree and custom table-cell export.
    Custom table-cell export strips replaces all *s with &nbsp;

--- settings/setup-org-mode.el
+++ settings/setup-org-mode.el
@@ -19,6 +19,53 @@
 (require 'org-bullets)
 (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
 
+(setq org-export-initial-scope 'subtree)
+(setq org-html-doctype "html5")
+
+
+(defun org-html-table-cell (table-cell contents info)
+  "Transcode a TABLE-CELL element from Org to HTML.
+CONTENTS is nil.  INFO is a plist used as a communication
+channel."
+  (let* ((table-row (org-export-get-parent table-cell))
+	 (table (org-export-get-parent-table table-cell))
+	 (cell-attrs-old
+	  (if (not org-html-table-align-individual-fields) ""
+	    (format (if (and (boundp 'org-html-format-table-no-css)
+			     org-html-format-table-no-css)
+			" align=\"%s\"" " class=\"%s\"")
+		    (org-export-table-cell-alignment table-cell info))))
+	 (cell-attrs "")
+         (cells-attrs-list '())
+         (key "")
+         (value "")
+         (key-and-value nil))
+    (if (not org-html-table-align-individual-fields) nil
+      (setq value (org-export-table-cell-alignment table-cell info))
+      (if (and (boundp 'org-html-format-table-no-css)
+               org-html-format-table-no-css)
+          (setq key "align")
+        (setq key "class"))
+      (push (cons key value) cells-attrs-list))
+    (when (or (not contents) (string= "" (org-trim contents)))
+      (setq contents "&#xa0;"))
+    (if (string-prefix-p "*" contents)
+        (setq contents (replace-regexp-in-string "\*" "&nbsp;" contents)))
+    (dolist (elt cells-attrs-list cell-attrs)
+      (setq key-and-value elt)
+      (setq cell-attrs (concat cell-attrs (format " %s=\"%s\"" (car key-and-value) (cdr key-and-value)))))
+    (cond
+     ((and (org-export-table-has-header-p table info)
+	   (= 1 (org-export-table-row-group table-row info)))
+      (concat "\n" (format (car org-html-table-header-tags) "col" cell-attrs)
+	      contents (cdr org-html-table-header-tags)))
+     ((and org-html-table-use-header-tags-for-first-column
+	   (zerop (cdr (org-export-table-cell-address table-cell info))))
+      (concat "\n" (format (car org-html-table-header-tags) "row" cell-attrs)
+	      contents (cdr org-html-table-header-tags)))
+     (t (concat "\n" (format (car org-html-table-data-tags) cell-attrs)
+		contents (cdr org-html-table-data-tags))))))
+
 ;;; Estimation
 
 ;; global Effort estimate values
