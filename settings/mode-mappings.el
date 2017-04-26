58c270764070d9278c6dfc4feb7c24141b79b723
Barrie Treloar <baerrach@gmail.com>
2017-04-27 09:21:53 +0930

    Remove duplicate for jsx pointing to incorrect web-mode.

--- settings/mode-mappings.el
+++ settings/mode-mappings.el
@@ -30,9 +30,6 @@
 (add-to-list 'auto-mode-alist '("\\.jsp$" . crappy-jsp-mode))
 (add-to-list 'auto-mode-alist '("\\.jspf$" . crappy-jsp-mode))
 
-;; JSX
-(add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
-
 ;; Ruby
 (autoload 'rhtml-mode "rhtml-mode")
 (add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
