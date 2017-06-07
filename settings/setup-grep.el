36c4e7959d2c386a5c5bdcf6ada5796bc1e6d34a
Barrie Treloar <baerrach@gmail.com>
2017-06-07 11:19:37 +0930

    Update grep to ignore more non-local source directories

--- settings/setup-grep.el
+++ settings/setup-grep.el
@@ -1,3 +1,11 @@
-(add-to-list 'grep-find-ignored-directories "elpa")
+(eval-after-load 'grep
+  '(progn
+     (add-to-list 'grep-find-ignored-directories "build")
+     (add-to-list 'grep-find-ignored-directories "elpa")
+     (add-to-list 'grep-find-ignored-directories "node_modules")
+     (add-to-list 'grep-find-ignored-directories "vendor")
+     ))
 
+(add-hook 'grep-mode-hook (lambda ()
+                            (visual-line-mode)))
 (provide 'setup-grep)
