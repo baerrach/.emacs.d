db351507546765c8023c33741094ae9a34c01899
Barrie Treloar <baerrach@gmail.com>
2017-06-07 11:20:10 +0930

    Upgrade from dabbrev to hippie-expand (case sensitive)

--- settings/key-bindings.el
+++ settings/key-bindings.el
@@ -1,5 +1,6 @@
 ;; Completion that uses many different methods to find options.
 (global-set-key (kbd "C-.") 'hippie-expand-no-case-fold)
+(global-set-key (kbd "M-/") 'hippie-expand-no-case-fold) ;; Override dabbrev key
 (global-set-key (kbd "C-:") 'hippie-expand-lines)
 (global-set-key (kbd "C-,") 'completion-at-point)
 
