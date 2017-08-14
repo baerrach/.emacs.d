bc3f0a980b39abbfe59a556e6a682a757703b4c7
Barrie Treloar <baerrach@gmail.com>
2017-08-15 08:47:33 +0930

    Don't downcase company matches.

--- settings/setup-company.el
+++ settings/setup-company.el
@@ -2,4 +2,8 @@
 
 (add-hook 'after-init-hook 'global-company-mode)
 
+;; Replacement text should not be downcased, it should use the case of the text
+;; it matched
+(setq company-dabbrev-downcase nil)
+
 (provide 'setup-company)
