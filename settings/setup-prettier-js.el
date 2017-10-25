2f43e888025cf39f2db0bef0a96319af95afa786
Barrie Treloar <baerrach@gmail.com>
2017-10-25 13:15:07 +1030

    Disable prettier, use manually.

--- settings/setup-prettier-js.el
+++ settings/setup-prettier-js.el
@@ -6,7 +6,8 @@
         "--trailing-comma" "es5"
         ))
 
-(add-hook 'js2-mode-hook 'prettier-js-mode)
-(add-hook 'js2-jsx-mode-hook 'prettier-js-mode)
+;; Disable Prettier for now
+;; (add-hook 'js2-mode-hook 'prettier-js-mode)
+;; (add-hook 'js2-jsx-mode-hook 'prettier-js-mode)
 
 (provide 'setup-prettier-js)
