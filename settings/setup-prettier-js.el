87fb6ac9a3c373d0df3fc1acaf79ed199aceb046
Barrie Treloar <baerrach@gmail.com>
2018-03-15 15:14:00 +1030

    Remove prettier config, that belongs in .prettierrc.

--- settings/setup-prettier-js.el
+++ settings/setup-prettier-js.el
@@ -1,11 +1,5 @@
 (require 'prettier-js)
 
-(setq prettier-js-args
-      '(
-        "--single-quote"
-        "--trailing-comma" "es5"
-        ))
-
 (add-hook 'js2-mode-hook 'prettier-js-mode)
 (add-hook 'js2-jsx-mode-hook 'prettier-js-mode)
 
