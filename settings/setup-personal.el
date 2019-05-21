a345806c3d3cddd7e82b7e88c8d6ac925aa42278
Barrie Treloar <barrie@mea.com.au>
2019-05-21 13:02:44 +0930

    Adjust fonts for 27" screens.

--- settings/setup-personal.el
+++ settings/setup-personal.el
@@ -1,13 +1,13 @@
 ;; friendly font
 (defun bae/work-settings ()
   (interactive)
-  (setq bae/default-font "-outline-Consolas-normal-normal-normal-mono-24-*-*-*-c-*-iso8859-1")
+  (setq bae/default-font "-outline-Consolas-normal-normal-normal-mono-22-*-*-*-c-*-iso8859-1")
   (set-face-attribute 'default nil :font bae/default-font)
   (set-face-attribute 'mode-line nil :font bae/default-font))
 
 (defun bae/home-settings ()
   (interactive)
-  (setq bae/default-font "-outline-Consolas-normal-normal-normal-mono-36-*-*-*-c-*-iso8859-1")
+  (setq bae/default-font "-outline-Consolas-normal-normal-normal-mono-24-*-*-*-c-*-iso8859-1")
   (set-face-attribute 'default nil :font bae/default-font)
   (set-face-attribute 'mode-line nil :font bae/default-font))
 
