36e373bee18387a57fae6891d322cfeb1efdd4e3
Barrie Treloar <baerrach@gmail.com>
2019-06-28 21:36:00 +0930

    Adjust fonts for home.

--- settings/setup-personal.el
+++ settings/setup-personal.el
@@ -7,7 +7,7 @@
 
 (defun bae/home-settings ()
   (interactive)
-  (setq bae/default-font "-outline-Consolas-normal-normal-normal-mono-24-*-*-*-c-*-iso8859-1")
+  (setq bae/default-font "-outline-Consolas-normal-normal-normal-mono-54-*-*-*-c-*-iso8859-1")
   (set-face-attribute 'default nil :font bae/default-font)
   (set-face-attribute 'mode-line nil :font bae/default-font))
 
