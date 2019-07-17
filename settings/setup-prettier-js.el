5c87f47455955912db2292838239c4a0aac9009e
Barrie Treloar <barrie@mea.com.au>
2019-07-17 15:08:40 +0930

    fix(prettier): only enable if .prettierrc.js exists and not in disabled directories

--- settings/setup-prettier-js.el
+++ settings/setup-prettier-js.el
@@ -3,7 +3,18 @@
 (add-hook 'js2-mode-hook #'add-node-modules-path)
 (add-hook 'js2-jsx-mode-hook #'add-node-modules-path)
 
-(add-hook 'js2-mode-hook 'prettier-js-mode)
-(add-hook 'js2-jsx-mode-hook 'prettier-js-mode)
+(defun bae/maybe-use-prettier ()
+  "Enable prettier-js-mode if an rc file is located."
+  (interactive)
+  (let* ((prettier-disabled (or (string-match-p (regexp-quote "node_modules") buffer-file-name)
+                                (string-match-p (regexp-quote "vendor") buffer-file-name)
+                                (string-match-p (regexp-quote "_template") buffer-file-name)))
+         (has-prettierrc (locate-dominating-file buffer-file-name ".prettierrc.js"))
+         (enable-prettier (and has-prettierrc (not prettier-disabled))))
+    (when enable-prettier
+      (prettier-js-mode 1))))
+
+(add-hook 'js2-mode-hook 'bae/maybe-use-prettier)
+(add-hook 'js2-jsx-mode-hook 'bae/maybe-use-prettier)
 
 (provide 'setup-prettier-js)
