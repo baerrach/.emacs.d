d904e05e21889509be046d4ae6d469fcddc6d508
Barrie Treloar <baerrach@gmail.com>
2019-08-22 07:58:26 +0930

    refactor(debug): change comment style

--- init.el
+++ init.el
@@ -1,6 +1,6 @@
 ;;; Enable for debugging problems
-;; (setq debug-on-quit t)
-;; (setq debug-on-error t)
+; (setq debug-on-quit t)
+; (setq debug-on-error t)
 
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
