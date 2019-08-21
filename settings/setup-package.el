e62aee2f9ff3e1c071e3a7c546ab5a7bbb4e3d31
Barrie Treloar <baerrach@gmail.com>
2019-08-21 21:48:55 +0930

    feat(use-package): turn off custom package handling
    In preparation for using use-package

--- settings/setup-package.el
+++ settings/setup-package.el
@@ -9,29 +9,8 @@
 (unless (file-exists-p "~/.emacs.d/elpa/archives/melpa")
   (package-refresh-contents))
 
-(if (not (package-installed-p 'dash))
-    (package-install 'dash))
-
-(require 'dash)
-
-(defun packages-install (packages)
-  (--each packages
-    (when (not (package-installed-p it))
-      (package-install it)))
-  (delete-other-windows))
-
-;;; On-demand installation of packages
-
-(defun require-package (package &optional min-version no-refresh)
-  "Install given PACKAGE, optionally requiring MIN-VERSION.
-If NO-REFRESH is non-nil, the available package lists will not be
-re-downloaded in order to locate PACKAGE."
-  (if (package-installed-p package min-version)
-      t
-    (if (or (assoc package package-archive-contents) no-refresh)
-        (package-install package)
-      (progn
-        (package-refresh-contents)
-        (require-package package min-version t)))))
+(when (not (package-installed-p 'use-package))
+  (package-install 'use-package))
+(delete-other-windows)
 
 (provide 'setup-package)
