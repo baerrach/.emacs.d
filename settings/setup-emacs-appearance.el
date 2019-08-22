04ab7273ff400560400bc5aba93cc22a163a358a
Barrie Treloar <baerrach@gmail.com>
2019-08-22 18:00:13 +0930

    refactor(appearance): move interface code earlier in init.el

--- settings/setup-emacs-appearance.el
+++ settings/setup-emacs-appearance.el
@@ -5,14 +5,6 @@
 ;;; https://github.com/magnars/.emacs.d/blob/master/init.el
 ;;;
 
-;; Turn off mouse interface early in startup to avoid momentary display
-(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
-(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
-(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
-
-;; No splash screen please ... jeez
-(setq inhibit-startup-message t)
-
 ;;; https://stackoverflow.com/questions/2081577/setting-emacs-split-to-horizontal
 ;;; Always split horizontally (i.e. windows are left/right, not top/bottom on top of each other)
 (setq split-height-threshold nil); originally 80
