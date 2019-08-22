c8ee5c3ca76e51c671976abb378f779abf13bf4b
Barrie Treloar <baerrach@gmail.com>
2019-08-22 19:53:53 +0930

    feat(emacs-server): use-package server

--- settings/setup-emacs-server.el
+++ settings/setup-emacs-server.el
@@ -1,6 +1,10 @@
-(require 'server)
-(unless (server-running-p)
-  (server-start))
+(require 'use-package)
+
 ;; Use emacsclientw.exe to open files in running server
+
+(use-package server
+  :config
+  (unless (server-running-p)
+    (server-start)))
 
 (provide 'setup-emacs-server)
