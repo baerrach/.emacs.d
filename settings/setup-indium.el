43933c3f95d5024309859d26fa08247afc346c16
Barrie Treloar <baerrach@gmail.com>
2019-08-26 18:09:53 +0930

    feat(indium): use-package indium

--- settings/setup-indium.el
+++ settings/setup-indium.el
@@ -1,24 +1,24 @@
-(require 'indium)
+(require 'use-package)
 
-(setq indium-nodejs-inspect-brk nil)
+(use-package indium
+  :hook (js2-mode . indium-interaction-mode)
+  :custom
+  (indium-nodejs-inspect-brk nil)
+  :config
+  ;; selenium-utils needs the REPL environment set to something to run the debug REPL loop.
+  (setenv "REPL" "1")
 
-;; selenium-utils needs the REPL environment set to something to run the debug
-;; REPL loop.
-(setenv "REPL" "1")
+  (defun bae/indium-backend-evaluate (orig-indium-backend-evaluate backend string &optional callback)
+    (when (string-match-p "^\\(const\\|let\\)\\W*" string)
+      (setq string (replace-regexp-in-string "^\\(const\\|let\\)\\W*" "" string)))
 
-(defun bae/indium-backend-evaluate (orig-indium-backend-evaluate backend string &optional callback)
-  (when (string-match-p "^\\(const\\|let\\)\\W*" string)
-    (setq string (replace-regexp-in-string "^\\(const\\|let\\)\\W*" "" string)))
+    (when (string-match-p "^\\(\\w+\\W*=\\)?\\W*await" string)
+      (message "Wrapping javascript in async block")
+      (setq string (format "(async () => {%s; console.log('Wrapped async completed.');})();" string)))
 
-  (when (string-match-p "^\\(\\w+\\W*=\\)?\\W*await" string)
-    (message "Wrapping javascript in async block")
-    (setq string (format "(async () => {%s; console.log('Wrapped async completed.');})();" string)))
+    (funcall orig-indium-backend-evaluate backend string callback)
+    )
 
-  (funcall orig-indium-backend-evaluate backend string callback)
-  )
-
-(advice-add 'indium-backend-evaluate :around #'bae/indium-backend-evaluate)
-
-(add-hook 'js2-mode-hook #'indium-interaction-mode)
+  (advice-add 'indium-backend-evaluate :around #'bae/indium-backend-evaluate))
 
 (provide 'setup-indium)
