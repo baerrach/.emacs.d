81f64615a3c3677508b3e27730cd3ceb8791def1
Barrie Treloar <barrie@mea.com.au>
2019-08-27 14:35:42 +0930

    feat(prettier-js): use-package prettier-js

--- settings/setup-prettier-js.el
+++ settings/setup-prettier-js.el
@@ -1,20 +1,20 @@
-(require 'prettier-js)
+(require 'use-package)
 
-(add-hook 'js2-mode-hook #'add-node-modules-path)
-(add-hook 'js2-jsx-mode-hook #'add-node-modules-path)
-
-(defun bae/maybe-use-prettier ()
-  "Enable prettier-js-mode if an rc file is located."
-  (interactive)
-  (let* ((prettier-disabled (or (string-match-p (regexp-quote "node_modules") buffer-file-name)
-                                (string-match-p (regexp-quote "vendor") buffer-file-name)
-                                (string-match-p (regexp-quote "_template") buffer-file-name)))
-         (has-prettierrc (locate-dominating-file buffer-file-name ".prettierrc.js"))
-         (enable-prettier (and has-prettierrc (not prettier-disabled))))
-    (when enable-prettier
-      (prettier-js-mode 1))))
-
-(add-hook 'js2-mode-hook 'bae/maybe-use-prettier)
-(add-hook 'js2-jsx-mode-hook 'bae/maybe-use-prettier)
+(use-package prettier-js
+  :after add-node-modules-path
+  :commands prettier-js-mode
+  :hook ((js2-mode js2-jsx-mode) . bae/maybe-use-prettier)
+  :config
+  (defun bae/maybe-use-prettier ()
+    "Enable prettier-js-mode if an rc file is located."
+    (interactive)
+    (let* ((prettier-disabled (or (string-match-p (regexp-quote "node_modules") buffer-file-name)
+                                  (string-match-p (regexp-quote "vendor") buffer-file-name)
+                                  (string-match-p (regexp-quote "_template") buffer-file-name)))
+           (has-prettierrc (locate-dominating-file buffer-file-name ".prettierrc.js"))
+           (enable-prettier (and has-prettierrc (not prettier-disabled))))
+      (when enable-prettier
+        (add-node-modules-path)
+        (prettier-js-mode 1)))))
 
 (provide 'setup-prettier-js)
