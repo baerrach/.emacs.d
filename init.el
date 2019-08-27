fa8971c50cd1bbac382bc0ae5c38d7fdca792936
Barrie Treloar <barrie@mea.com.au>
2019-08-27 16:10:16 +0930

    feat(smooth-scrolling): use-package smooth-scrolling

--- init.el
+++ init.el
@@ -186,9 +186,9 @@
 (require 'setup-prettier-js)
 (require 'setup-shell)
 ;; TODO - UP TO HERE
-(require 'smooth-scrolling)
 (require 'setup-smart-forward)
 (require 'setup-smex)
+(require 'setup-smooth-scrolling)
 (require 'string-inflection)
 (require 'setup-tide)
 (require 'visual-basic-mode)
---
+++ settings/setup-smooth-scrolling.el
@@ -0,0 +1,8 @@
+(require 'use-package)
+
+(use-package smooth-scrolling
+  :ensure
+  :config
+  (smooth-scrolling-mode 1))
+
+(provide 'setup-smooth-scrolling)
