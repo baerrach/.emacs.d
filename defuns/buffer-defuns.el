913375b9d3bc837e210753274f962f931f13e259
Barrie Treloar <baerrach@gmail.com>
2019-08-28 21:55:09 +0930

    refactor(key-binding): remove defuns not being used and their bindings

--- defuns/buffer-defuns.el
+++ defuns/buffer-defuns.el
@@ -1,21 +1,5 @@
 ;; Buffer-related defuns
 
-(defun untabify-buffer ()
-  (interactive)
-  (untabify (point-min) (point-max)))
-
-(defun indent-buffer ()
-  (interactive)
-  (indent-region (point-min) (point-max)))
-
-(defun cleanup-buffer ()
-  "Perform a bunch of operations on the whitespace content of a buffer.
-Including indent-buffer, which should not be called automatically on save."
-  (interactive)
-  (untabify-buffer)
-  (delete-trailing-whitespace)
-  (indent-buffer))
-
 (defun goto-line-with-feedback ()
   "Show line numbers temporarily, while prompting for the line number input"
   (interactive)
@@ -24,19 +8,3 @@ Including indent-buffer, which should not be called automatically on save."
         (linum-mode 1)
         (call-interactively 'goto-line))
     (linum-mode -1)))
-
-(defvar user-home-directory (concat (expand-file-name "~") "/"))
-
-(defun shorter-file-name (file-name)
-  (s-chop-prefix user-home-directory file-name))
-
-(defun recentf--file-cons (file-name)
-  (cons (shorter-file-name file-name) file-name))
-
-(defun recentf-ido-find-file ()
-  "Find a recent file using ido."
-  (interactive)
-  (let* ((recent-files (mapcar 'recentf--file-cons recentf-list))
-         (files (mapcar 'car recent-files))
-         (file (completing-read "Choose recent file: " files)))
-    (find-file (cdr (assoc file recent-files)))))
--- settings/key-bindings.el
+++ settings/key-bindings.el
@@ -1,23 +1,13 @@
-;; Perform general cleanup.
-(global-set-key (kbd "C-c n") 'cleanup-buffer)
-(global-set-key (kbd "C-c C-n") 'cleanup-buffer)
-(global-set-key (kbd "C-c C-<return>") 'delete-blank-lines)
-
 ;; M-i for back-to-indentation
 (global-set-key (kbd "M-i") 'back-to-indentation)
 
 ;; Shift-tab for indent-rigidly
 (global-set-key (kbd "S-<tab>") 'indent-rigidly)
 
-;; Turn on the menu bar for exploring new modes
-(global-set-key (kbd "C-<f10>") 'menu-bar-mode)
-
 ;; Use shell-like backspace C-h, rebind help to F1
 (define-key key-translation-map [?\C-h] [?\C-?])
 (global-set-key (kbd "<f1>") 'help-command)
 (global-unset-key (kbd "<f1> h")) ;; Don't need view-hello-file
-
-(global-set-key (kbd "M-h") 'kill-region-or-backward-word)
 
 ;; Transpose stuff with M-t
 (global-unset-key (kbd "M-t")) ;; which used to be transpose-words
@@ -26,10 +16,6 @@
 (global-set-key (kbd "M-t s") 'transpose-sexps)
 (global-set-key (kbd "M-t p") 'transpose-params)
 
-;; Change next underscore with a camel case
-(global-set-key (kbd "C-c C--") 'replace-next-underscore-with-camel)
-(global-set-key (kbd "M-s M--") 'snakeify-current-word)
-
 ;; Change word separators
 (global-unset-key (kbd "C-x +")) ;; used to be balance-windows
 (global-set-key (kbd "C-x + -") (λ (replace-region-by 's-dashed-words)))
@@ -37,17 +23,10 @@
 (global-set-key (kbd "C-x + c") (λ (replace-region-by 's-lower-camel-case)))
 (global-set-key (kbd "C-x + C") (λ (replace-region-by 's-upper-camel-case)))
 
-;; Jump to a definition in the current file. (This is awesome)
-(global-set-key (kbd "C-x C-i") 'ido-imenu)
-
 ;; File finding
 (global-set-key (kbd "C-x M-f") 'ido-find-file-other-window)
-(global-set-key (kbd "C-x f") 'recentf-ido-find-file)
-(global-set-key (kbd "C-x C-p") 'find-or-create-file-at-point)
-(global-set-key (kbd "C-x M-p") 'find-or-create-file-at-point-other-window)
 (global-set-key (kbd "C-c y") 'bury-buffer)
 (global-set-key (kbd "C-c r") 'revert-buffer)
-(global-set-key (kbd "M-`") 'file-cache-minibuffer-complete)
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 
 ;; Help should search more than just commands
@@ -62,11 +41,6 @@
 (global-set-key (kbd "M-p") 'backward-paragraph)
 (global-set-key (kbd "M-n") 'forward-paragraph)
 
-(global-set-key (kbd "M-<up>") 'smart-up)
-(global-set-key (kbd "M-<down>") 'smart-down)
-(global-set-key (kbd "M-<left>") 'smart-backward)
-(global-set-key (kbd "M-<right>") 'smart-forward)
-
 ;; Completion at point
 (global-set-key (kbd "C-<tab>") 'completion-at-point)
 
@@ -76,22 +50,12 @@
 (global-set-key (kbd "C-S-f") (λ (ignore-errors (forward-char 5))))
 (global-set-key (kbd "C-S-b") (λ (ignore-errors (backward-char 5))))
 
-;; Query replace regex key binding
-(global-set-key (kbd "M-&") 'query-replace-regexp)
-
-;; Yank selection in isearch
-(define-key isearch-mode-map (kbd "C-o") 'isearch-yank-selection)
-
 ;; Comment/uncomment block
 (global-set-key (kbd "C-c c") 'comment-or-uncomment-region)
 (global-set-key (kbd "C-c u") 'uncomment-region)
 
-;; Toggle quotes
-(global-set-key (kbd "C-\"") 'toggle-quotes)
-
 ;; Sorting
 (global-set-key (kbd "M-s l") 'sort-lines)
-
 
 ;; I don't need to kill emacs that easily
 ;; the mnemonic is C-x REALLY QUIT
@@ -103,16 +67,6 @@
 
 ;; Unbind commands that get invoked accidentally and will never be used.
 (global-unset-key (kbd "C-x C-n"))
-
-;;; https://emacs.stackexchange.com/a/7925/12751
-;; Note the addition of magit-mode check and using ido-complete in the minibuffer.
-(defun check-expansion ()
-  (save-excursion
-    (if (looking-at "\\_>") t
-      (backward-char 1)
-      (if (looking-at "\\.") t
-    (backward-char 1)
-    (if (looking-at "->") t nil)))))
 
 ;;; How to create keyboard macros
 ;; start-kbd-macro
