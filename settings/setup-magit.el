70a2d640a9087219ed4d861aa207ffcba2166256
Barrie Treloar <baerrach@gmail.com>
2019-08-30 16:48:13 +0930

    fix(magit): disable-on-error around correct function

--- settings/setup-magit.el
+++ settings/setup-magit.el
@@ -21,14 +21,16 @@
     (when (looking-at "Head")
       (magit-section-forward-sibling)))
 
-  (defun disable-debug-on-error--around-with-editor-finish (orig-fun &rest args)
-    "Disable debug-on-error around with-editor-finish.
+  (defun disable-debug-on-error--backup-buffer-copy (orig-fun &rest args)
+    "Disable debug-on-error around backup-buffer-copy.
+
+See https://bug-gnu-emacs.gnu.narkive.com/pRQgqRm6/bug-29455-backup-by-copying-acl-operation-not-permitted-with-windows-7-and-samba
 
 Saving files soft handles errors and promotes them to warnings.
 But with debug-on-error enabled it causes the debugger to open
 instead of ignoring what should be a warning."
-    (let (debug-on-error nil)
+    (let ((debug-on-error nil))
       (apply orig-fun args)))
-  (advice-add 'with-editor :around #'disable-debug-on-error--around-with-editor-finish))
+  (advice-add 'backup-buffer-copy :around #'disable-debug-on-error--backup-buffer-copy))
 
 (provide 'setup-magit)
