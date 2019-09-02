9e85c2a8217b80958409c5ce317957459789947a
Barrie Treloar <barrie@mea.com.au>
2019-09-02 13:53:48 +0930

    feat(projectile): teach projectile about aurelia projects

--- settings/setup-projectile.el
+++ settings/setup-projectile.el
@@ -7,24 +7,22 @@
               ("C-c C-p" . projectile-command-map))
   :custom
   (projectile-completion-system 'ivy)
-  (projectile-indexing-method 'alien))
+  (projectile-indexing-method 'alien)
+  ;;   "Teach Projectile about Aurelia related files:
+  ;;
+  ;; * View (.html) -> ViewModel (.js)
+  (projectile-aurelia-related-files
+   (list
+    (projectile-related-files-fn-extensions :other '("html" "js"))))
+  :config
+  (projectile-register-project-type 'aurelia '("aurelia_project/aurelia.json")
+                                    :compile nil
+                                    :test "npm test"
+                                    :run "npm start"
+                                    :test-suffix ".spec"
+                                    :src-dir "src/"
+                                    :test-dir "test/"
+                                    :related-files-fn projectile-aurelia-related-files)
+  )
 
 (provide 'setup-projectile)
-
-;; ;; Need to configure to define spec files, its set to NPM -> make it Aurelia
-;; (defun my/related-files (&optional path)
-;;   (if (and path (string-match (rx (group (or "src" "test")) (group "/" (1+ anything) ".cpp")) path))
-;;       (let ((dir (match-string 1 path))
-;;             (file-name (match-string 2 path)))
-;;         (if (equal dir "test")
-;;             (list :impl (concat "src" file-name))
-;;           (list :test (concat "test" file-name)
-;;                 :other (concat "src" file-name ".def"))))))
-
-;; (projectile-register-project-type
-;;  ;; ...
-;;  :related-files-fn #'my/related-files)
-
-
-;; (add-to-list 'projectile-other-file-alist '("js" "html"))
-;; (add-to-list 'projectile-other-file-alist '("html" "js"))
