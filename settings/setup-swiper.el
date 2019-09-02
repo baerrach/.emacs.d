9353a454b29978c3e66a1869924532eb7aa017a1
Barrie Treloar <barrie@mea.com.au>
2019-09-02 14:18:54 +0930

    feat(swiper): bind C-r to swiper

--- settings/setup-swiper.el
+++ settings/setup-swiper.el
@@ -3,6 +3,9 @@
 (use-package swiper
   :ensure
   :after ivy
-  :bind (("C-s" . swiper)))
+  :bind (("C-s" . swiper)
+         ;; swiper opens a minbuffer it has no direction!
+         ;; it automatically focuses on the current line
+         ("C-r" . swiper)))
 
 (provide 'setup-swiper)
