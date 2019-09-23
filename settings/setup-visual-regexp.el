eb831db5e7d1e7a6049b619c58b835253571992b
Barrie Treloar <barrie@mea.com.au>
2019-09-24 09:20:00 +0930

    feat(visual-regexp): remove C-C r as conflicts with revert buffer

--- settings/setup-visual-regexp.el
+++ settings/setup-visual-regexp.el
@@ -2,8 +2,7 @@
 
 (use-package visual-regexp
   :ensure
-  :bind (("C-c r" . vr/replace)
-         ("C-c q" . vr/query-replace)
+  :bind (("C-c q" . vr/query-replace)
          ;; if you use multiple-cursors, this is for you:
          ("C-c m" . vr/mc-mark)))
 
