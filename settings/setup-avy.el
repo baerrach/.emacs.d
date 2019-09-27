290aae27e0ddde8642d8b4adae5fa2dded601843
Barrie Treloar <barrie@mea.com.au>
2019-09-27 13:52:25 +0930

    fix(avy): map "C-C C-<SPC>" to be same as "C-C <SPC>"

--- settings/setup-avy.el
+++ settings/setup-avy.el
@@ -4,6 +4,7 @@
   :ensure
   :bind (("C-c C-j" . avy-resume)
          ("C-c <SPC>" . avy-goto-char)
+         ("C-c C-<SPC>" . avy-goto-char)
          :map isearch-mode-map
               ("C-'" . avy-isearch)))
 
