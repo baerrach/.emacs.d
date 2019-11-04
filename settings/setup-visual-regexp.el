38d812f13cf311a7b2ab9f7a032a7b0cd3fb32c1
Barrie Treloar <barrie@mea.com.au>
2019-11-04 13:02:20 +1030

    feat(visual-regexp): bind to C-c C-q as well
    Always hitting with `C-` enabled.

--- settings/setup-visual-regexp.el
+++ settings/setup-visual-regexp.el
@@ -3,6 +3,7 @@
 (use-package visual-regexp
   :ensure
   :bind (("C-c q" . vr/query-replace)
+         ("C-c C-q" . vr/query-replace)
          ;; if you use multiple-cursors, this is for you:
          ("C-c m" . vr/mc-mark)))
 
