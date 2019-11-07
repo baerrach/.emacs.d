6e8eb657a6faaf658cc3ac52f6102a633e3294f8
Barrie Treloar <barrie@mea.com.au>
2019-11-07 11:33:04 +1030

    feat(counsel-projectile): add rename to find-file-action list

--- settings/setup-counsel-projectile.el
+++ settings/setup-counsel-projectile.el
@@ -2,7 +2,27 @@
 
 (use-package counsel-projectile
   :after counsel projectile
+  :custom
+  (counsel-projectile-find-file-action '(1
+                                         ("o" counsel-projectile-find-file-action
+                                          "current window")
+                                         ("j" counsel-projectile-find-file-action-other-window
+                                          "other window")
+                                         ("x" counsel-projectile-find-file-action-extern
+                                          "open externally")
+                                         ("R" bae/counsel-projectile-find-file-action-move
+                                          "rename or move")
+                                         ("m" counsel-projectile-find-file-action-find-file-manually
+                                          "find file manually")
+                                         ("k" counsel-projectile-find-file-action-delete
+                                          "delete")
+                                         ("p" counsel-projectile-find-file-action-switch-project
+                                          "switch project")))
   :config
+  (defun bae/counsel-projectile-find-file-action-move (file)
+    "Move or rename file FILE."
+    (counsel-find-file-move (projectile-expand-root file)))
+
   (counsel-projectile-mode 1))
 
 (provide 'setup-counsel-projectile)
