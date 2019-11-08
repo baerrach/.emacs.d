13d90a61177c09d888fdd57db5200758ec091b8e
Barrie Treloar <barrie@mea.com.au>
2019-11-08 13:05:01 +1030

    feat(counsel-projectile): patch counsel-find-file-move to make parent directory

--- settings/setup-counsel-projectile.el
+++ settings/setup-counsel-projectile.el
@@ -1,7 +1,7 @@
 (require 'use-package)
 
 (use-package counsel-projectile
-  :after counsel projectile
+  :after (counsel projectile el-patch)
   :custom
   (counsel-projectile-find-file-action '(1
                                          ("o" counsel-projectile-find-file-action
@@ -19,6 +19,21 @@
                                          ("p" counsel-projectile-find-file-action-switch-project
                                           "switch project")))
   :config
+  (el-patch-feature counsel-projectile)
+  (with-eval-after-load 'counsel-projectile
+    (el-patch-defun counsel-find-file-move (x)
+      "Move or rename file X."
+      (require 'dired-aux)
+      (counsel--find-file-1 "Rename file to: "
+                            (el-patch-swap ivy--directory x)
+                            (lambda (new-name)
+                              (el-patch-add
+                                (let ((dir (file-name-directory new-name)))
+                                  (if (not (file-directory-p dir))
+                                    (make-directory dir t))))
+                              (dired-rename-file x new-name 1))
+                            'counsel-find-file-move)))
+
   (defun bae/counsel-projectile-find-file-action-move (file)
     "Move or rename file FILE."
     (counsel-find-file-move (projectile-expand-root file)))
