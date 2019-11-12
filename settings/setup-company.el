627cc6ae6a397b4a2c2b31cf65041eb0a5752f87
Barrie Treloar <barrie@mea.com.au>
2019-11-12 12:50:52 +1030

    feat(company): improve dabbrev

--- settings/setup-company.el
+++ settings/setup-company.el
@@ -11,7 +11,9 @@
          ("C-n" . company-select-next)
          ("C-p" . company-select-previous))
   :custom
+  (company-dabbrev-other-buffers nil "Only search the current buffer to get suggestions for. Too many buffers causes lag")
   (company-dabbrev-downcase nil "Replacement text should not be downcased, it should use the case of the text it matched")
+  (company-dabbrev-ignore-case "Replacement text should match candidate, even if prefix is different")
   (company-idle-delay nil "only show company on manual invocation"))
 
 (provide 'setup-company)
