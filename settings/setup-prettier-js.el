4231bd6aa17cbacc4e0d60eb8fe92345c81ecf75
Barrie Treloar <barrie@mea.com.au>
2019-12-11 09:29:09 +1030

    feat(prettier): add json-mode, remove config file check
    When using shared configs the config file check no longer is valid.

--- settings/setup-prettier-js.el
+++ settings/setup-prettier-js.el
@@ -4,7 +4,7 @@
   :after (js2-mode add-node-modules-path)
   :commands prettier-js-mode
   :diminish
-  :hook ((js2-mode js2-jsx-mode) . bae/maybe-use-prettier)
+  :hook ((js2-mode js2-jsx-mode json-mode) . bae/maybe-use-prettier)
   :config
   (defun bae/maybe-use-prettier ()
     "Enable prettier-js-mode if an rc file is located."
@@ -12,8 +12,7 @@
     (let* ((prettier-disabled (or (string-match-p (regexp-quote "node_modules") buffer-file-name)
                                   (string-match-p (regexp-quote "vendor") buffer-file-name)
                                   (string-match-p (regexp-quote "_template") buffer-file-name)))
-           (has-prettierrc (locate-dominating-file buffer-file-name ".prettierrc.js"))
-           (enable-prettier (and has-prettierrc (not prettier-disabled))))
+           (enable-prettier (not prettier-disabled)))
       (when enable-prettier
         (add-node-modules-path)
         (prettier-js-mode 1)))))
