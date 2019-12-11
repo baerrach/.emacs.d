5286c6ed6f944d492901b68df1fd9bfc7708d0bb
Barrie Treloar <barrie@mea.com.au>
2019-12-11 17:24:31 +1030

    feat(smartparens): add key bindings

--- settings/setup-smartparens.el
+++ settings/setup-smartparens.el
@@ -2,8 +2,15 @@
 
 (use-package smartparens
   :diminish
+  :hook ((emacs-lisp-mode groovy-mode java-mode js-mode markdown-mode restclient-mode ruby-mode scala-mode) . turn-on-smartparens-mode)
+  :bind (("C-<down>" . sp-down-sexp)
+         ("C-<up>" . sp-up-sexp)
+         ("C-<left>" . sp-forward-sexp)
+         ("C-<right>" . sp-backward-sexp)
+         ("C-M-f" . sp-forward-sexp)
+         ("C-M-b" . sp-backward-sexp)
+         )
   :custom
   (sp-autoescape-string-quote nil)
-  :hook ((emacs-lisp-mode groovy-mode java-mode js-mode markdown-mode restclient-mode ruby-mode scala-mode) . turn-on-smartparens-mode))
 
 (provide 'setup-smartparens)
