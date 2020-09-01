b1730aca37cc88eeb64305680f1312badfe60fb7
Barrie Treloar <treloarb@dtup.sa.gov.au>
2020-09-01 10:36:51 +0930

    fix: expand-region must come after org
    or else it uses the built-in emacs version which is old.

--- settings/setup-expand-region.el
+++ settings/setup-expand-region.el
@@ -1,6 +1,7 @@
 (require 'use-package)
 
 (use-package expand-region
+  :after (org)
   :bind (;; Expand region (increases selected region by semantic units)
          ("C-'" . 'er/expand-region)))
 
