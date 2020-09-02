cceb6a9dbca437a1858d3bdcf43a18855a535d6f
Barrie Treloar <treloarb@dtup.sa.gov.au>
2020-09-02 11:24:58 +0930

    feat(win): attempt to get shell working correctly

--- settings/setup-microsoft-windows.el
+++ settings/setup-microsoft-windows.el
@@ -1,11 +1,11 @@
 (when (equal system-type 'windows-nt)
   ;; When running in Windows, we use Git Bash as alternate shell
   (setq explicit-shell-file-name "C:/Program Files/Git/bin/bash.exe")
-  (setq shell-file-name explicit-shell-file-name)
+  (setq shell-file-name "bash")
+  (setq explicit-bash.exe-args '("--noediting" "--login" "-i"))
+  (setenv "SHELL" "bash")
   ;; Tramp is used by magit
   (setq tramp-encoding-shell "C:/Program Files/Git/bin/bash.exe")
-  (setenv "SHELL" "bash")
-  (setq explicit-bash.exe-args '("--noediting" "-i"))
   (setq w32-quote-process-args ?\")
   (setq null-device "/dev/null")
 
