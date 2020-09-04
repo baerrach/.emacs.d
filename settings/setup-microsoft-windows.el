b9a53f1f609157d245049467562b85fff7826856
Barrie Treloar <treloarb@dtup.sa.gov.au>
2020-09-04 10:47:54 +0930

    fix(win): maybe really fix shell on windows

--- settings/setup-microsoft-windows.el
+++ settings/setup-microsoft-windows.el
@@ -1,11 +1,15 @@
 (when (equal system-type 'windows-nt)
   ;; When running in Windows, we use Git Bash as alternate shell
   ;; See M-x describe-function shell for full details on these variables
-  (setq explicit-shell-file-name "C:/Program Files/Git/bin/bash.exe")
-  (setq shell-file-name explicit-shell-file-name)
+  ;;
+  ;; Don't use explicit-shell-file-name as that will be a fully qualified path
+  ;; that some commands will dislike the spaces in the directories.
+  ;;
+  ;; Ensure that your %PATH% is setup to include C:/Program Files/Git/bin/
+  (setq shell-file-name "bash")
   (setq explicit-bash.exe-args '("--noediting" "--login" "-i"))
   ;; Tramp is used by magit
-  (setq tramp-encoding-shell "C:/Program Files/Git/bin/bash.exe")
+  (setq tramp-encoding-shell "bash")
   (setq w32-quote-process-args ?\")
   (setq null-device "/dev/null")
 
