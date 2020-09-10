619bb19ed3f6b99614bbe78db98ec1ccd208435e
Barrie Treloar <treloarb@dtup.sa.gov.au>
2020-09-10 13:45:19 +0930

    feat(uniquify): add defer to use-package

--- settings/setup-uniquify.el
+++ settings/setup-uniquify.el
@@ -3,6 +3,8 @@
 (straight-use-package '(uniquify :type built-in))
 
 (use-package uniquify
+  ;; Loads after 1 second of idle time.
+  :defer 1
   :custom
   (uniquify-buffer-name-style 'forward "Add parts of each file's directory to the buffer name if not unique"))
 
