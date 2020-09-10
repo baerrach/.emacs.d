8831430fef1c15b2ad92d1b8b3d0e8c50b5c09a1
Barrie Treloar <treloarb@dtup.sa.gov.au>
2020-09-10 13:18:57 +0930

    feat(emacs-server): add defer to use-package

--- settings/setup-emacs-server.el
+++ settings/setup-emacs-server.el
@@ -3,6 +3,8 @@
 ;; Use emacsclientw.exe to open files in running server
 
 (use-package server
+  ;; Loads after 2 second of idle time.
+  :defer 2
   :config
   (unless (server-running-p)
     (server-start)))
