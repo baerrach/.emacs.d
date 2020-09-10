2517a2af2cb1256b0953e20fae3b199023c6f40b
Barrie Treloar <treloarb@dtup.sa.gov.au>
2020-09-10 13:44:09 +0930

    feat(smartparens): add defer to use-package

--- settings/setup-smartparens.el
+++ settings/setup-smartparens.el
@@ -5,13 +5,12 @@
   :defer 1
   :diminish
   :hook ((emacs-lisp-mode groovy-mode java-mode js-mode markdown-mode restclient-mode ruby-mode scala-mode) . turn-on-smartparens-mode)
-  :bind (("C-<down>" . sp-down-sexp)
-         ("C-<up>" . sp-up-sexp)
-         ("C-<left>" . sp-forward-sexp)
-         ("C-<right>" . sp-backward-sexp)
+  :bind (("C-<down>" . sp-up-sexp)
+         ("C-<up>" . sp-down-sexp)
+         ("C-<left>" . sp-backward-sexp)
+         ("C-<right>" . sp-forward-sexp)
          ("C-M-f" . sp-forward-sexp)
-         ("C-M-b" . sp-backward-sexp)
-         )
+         ("C-M-b" . sp-backward-sexp))
   :custom
   (sp-autoescape-string-quote nil)
   :config
