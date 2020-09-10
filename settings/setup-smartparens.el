4658758e034999dab2a86b3daded784eec60785e
Barrie Treloar <treloarb@dtup.sa.gov.au>
2020-09-10 13:26:35 +0930

    feat(smartparens): add defer to use-package

--- settings/setup-smartparens.el
+++ settings/setup-smartparens.el
@@ -1,6 +1,8 @@
 (require 'use-package)
 
 (use-package smartparens
+  ;; Loads after 1 second of idle time.
+  :defer 1
   :diminish
   :hook ((emacs-lisp-mode groovy-mode java-mode js-mode markdown-mode restclient-mode ruby-mode scala-mode) . turn-on-smartparens-mode)
   :bind (("C-<down>" . sp-down-sexp)
